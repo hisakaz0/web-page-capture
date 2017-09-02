3798da49aff9f16964645c027b6bc1aa0a2e86fe
pinkienort <cantabilehisa@gmail.com>
2017-09-02 22:28:42 +0900

    add list loop(incomplete)

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const CDP = require('chrome-remote-interface');
 const util = require('util');
 const fs = require('fs');
 const config = require('config');
+const yaml = require('js-yaml');
 
 const writeFile = util.promisify(fs.writeFile);
 
@@ -11,7 +12,6 @@ async function setDeviceSize(Emulation, deviceMetrics) {
   await Emulation.setDeviceMetricsOverride(deviceMetrics);
   await Emulation.setVisibleSize({ width, height });
 }
-
 // ブラウザ内でページの最下部まで少しずつスクロールするためのメソッド
 const scrollToBottom = () => new Promise((resolve) => {
   const interval = setInterval(() => {
@@ -35,7 +35,6 @@ const getFullPageSize = () => JSON.stringify({
 // ブラウザ内で実行するために文字列化
 const getFullPageSizeScript = `(${getFullPageSize.toString()})()`;
 
-
 async function main() {
   // Chromeを起動
   const launcher = new Launcher(config.get('chrome.launcher'));
@@ -50,29 +49,34 @@ async function main() {
   const deviceMetrics = config.get('chrome.device');
   await setDeviceSize(Emulation, deviceMetrics);
 
-  // ページにアクセスして読み込みが完了するまで待機
-  Page.navigate({ url: 'https://www.pixiv.net/' });
-  await Page.loadEventFired();
+  // To capture web pages of the yaml list
+  const pagesList = yaml.safeLoad(fs.readFileSync('pages-list.yml', 'utf8'));
 
-  // ページの最下部までスクロール
-  await Runtime.evaluate({ expression: scrollToBottomScript, awaitPromise: true });
+  pagesList.forEach(async function(currentValue, index, array) {
+    // ページにアクセスして読み込みが完了するまで待機
+    Page.navigate({ url: currentValue.url });
+    await Page.loadEventFired();
 
-  // ブラウザからscrollWidthとscrollHeightを取得
-  const { result: { value } } = await Runtime.evaluate({
-    expression: getFullPageSizeScript,
-    returnByValue: true
+    // ページの最下部までスクロール
+    await Runtime.evaluate({ expression: scrollToBottomScript, awaitPromise: true });
+
+    // ブラウザからscrollWidthとscrollHeightを取得
+    const { result: { value } } = await Runtime.evaluate({
+      expression: getFullPageSizeScript,
+      returnByValue: true
+    });
+    const { width, height } = JSON.parse(value);
+
+    // デバイスのサイズをページのサイズに合わせる
+    await setDeviceSize(Emulation, Object.assign({}, deviceMetrics, {
+      width: Math.max(width, deviceMetrics.width),
+      height: Math.max(height, deviceMetrics.height)
+    }));
+
+    // スクリーンショットを取得、保存
+    const { data } = await Page.captureScreenshot({ fromSurface: true });
+    await writeFile(currentValue.imagePath, Buffer.from(data, 'base64'));
   });
-  const { width, height } = JSON.parse(value);
-
-  // デバイスのサイズをページのサイズに合わせる
-  await setDeviceSize(Emulation, Object.assign({}, deviceMetrics, {
-    width: Math.max(width, deviceMetrics.width),
-    height: Math.max(height, deviceMetrics.height)
-  }));
-
-  // スクリーンショットを取得、保存
-  const { data } = await Page.captureScreenshot({ fromSurface: true });
-  await writeFile('capture.png', Buffer.from(data, 'base64'));
 
   // 終了処理
   client.close();
